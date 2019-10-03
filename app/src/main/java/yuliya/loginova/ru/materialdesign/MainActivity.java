7168811a7bb5440ece5a8cf1bc78666fa57b3332
iulkolog <iulkolog@gmail.com>
2019-10-03 16:31:48 +0300

    Initial commit

---
+++ app/src/main/java/yuliya/loginova/ru/materialdesign/MainActivity.java
@@ -0,0 +1,14 @@
+package yuliya.loginova.ru.materialdesign;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
